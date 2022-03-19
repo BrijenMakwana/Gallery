75869bd00e6395266a8ca2fff1f7ec65fcbb44de
Brijen Makwana <brijenmakwana@Brijens-MacBook-Air.local>
2022-03-19 12:30:50 +0530

    Images coming from Sanity

--- src/components/gallery/Gallery.js
+++ src/components/gallery/Gallery.js
@@ -3,21 +3,21 @@ import "./Gallery.css";
 import ImageItem from "../imageItem/ImageItem";
 
 export default function ProjectList(props) {
-  const [projects, setProjects] = useState([]);
+  const [gallery, setGallery] = useState([]);
 
   const getProjects = () => {
     fetch(
-      "https://gzsq4ssh.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%3D%3D%22projects%22%5D%7B%0A%20%20title%2C%0A%20%20%22imageUrl%22%3A%20image.asset-%3Eurl%2C%0A%20%20description%2C%0A%20%20url%0A%7D"
+      "https://tqm1lism.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%3D%3D%22gallery%22%5D%20%7C%20order(_createdAt%20asc)%7B%0A%20%0A%20%20%22imageUrl%22%3A%20image.asset-%3Eurl%2C%0A%7D"
     )
       .then((res) => res.json())
       .then((res) => {
-        setProjects(res.result);
+        setGallery(res.result);
       });
   };
 
   useEffect(() => {
     getProjects();
-  }, []);
+  });
 
   return (
     <div className="g">
@@ -26,9 +26,9 @@ export default function ProjectList(props) {
         {/* <p className="pl-desc">{props.description}</p> */}
       </div>
       <div className="g-list">
-        {projects &&
-          projects.map((item, index) => (
-            <ImageItem key={index} imageUrl={item.imageUrl} url={item.url} />
+        {gallery &&
+          gallery.map((item, index) => (
+            <ImageItem key={index} imageUrl={item.imageUrl} />
           ))}
       </div>
     </div>
