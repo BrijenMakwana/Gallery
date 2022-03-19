e104e91d7867ddf0316238a4b18ca82c04922cfd
Brijen Makwana <brijenmakwana@Brijens-MacBook-Air.local>
2022-03-19 17:04:20 +0530

    Personal details coming from Sanity

--- src/components/gallery/Gallery.js
+++ src/components/gallery/Gallery.js
@@ -4,8 +4,19 @@ import ImageItem from "../imageItem/ImageItem";
 
 export default function ProjectList(props) {
   const [gallery, setGallery] = useState([]);
+  const [personal, setPersonal] = useState([]);
 
-  const getProjects = () => {
+  const getPersonalDetails = () => {
+    fetch(
+      "https://tqm1lism.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%3D%3D%22introduction%22%5D"
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        setPersonal(res.result[0]);
+      });
+  };
+
+  const getGallery = () => {
     fetch(
       "https://tqm1lism.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%3D%3D%22gallery%22%5D%20%7C%20order(_createdAt%20asc)%7B%0A%20%0A%20%20%22imageUrl%22%3A%20image.asset-%3Eurl%2C%0A%7D"
     )
@@ -16,14 +27,24 @@ export default function ProjectList(props) {
   };
 
   useEffect(() => {
-    getProjects();
-  });
+    getPersonalDetails();
+    getGallery();
+  }, []);
 
   return (
     <div className="g">
       <div className="g-texts">
-        <h1 className="g-title">Brijen Makwana</h1>
-        {/* <p className="pl-desc">{props.description}</p> */}
+        <h1 className="g-title">{personal.name}</h1>
+        <p className="g-desc">
+          <b>Hello There! ✌🏻</b>
+          <br />
+          <ul>
+            {personal.about &&
+              personal.about
+                .split(",")
+                .map((item, index) => <li key={index}>{item}</li>)}
+          </ul>
+        </p>
       </div>
       <div className="g-list">
         {gallery &&
